22e5a8eeb79abbe6b83b7bac2b4ceddcef89f4b6
Daniel Chukwurah <dnchukwurah@sidmach.com>
2020-04-09 14:49:41 +0100

    Fix indentation

--- src/estimator.js
+++ src/estimator.js
@@ -1,10 +1,13 @@
 const covid19ImpactEstimator = (data) => {
-    const input = data;
-    return {
-        data: input,
-        impact: {},
-        severeImpact: {}
-    }
+  const input = data;
+  return {
+    data: {},
+    impact: {},
+    severeImpact: {}
+  }
+
 };
+impact.currentlyInfected = reportedCases * 10;
+severeImpact.currentlyInfected = reportedCases * 50
 
 export default covid19ImpactEstimator;
