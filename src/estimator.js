cb4630a67f6fe278d777cce41e02b4ce9bb828d0
Daniel Chukwurah <dnchukwurah@sidmach.com>
2020-04-09 18:06:02 +0100

    Finished challenge one

--- src/estimator.js
+++ src/estimator.js
@@ -1,11 +1,41 @@
+const getInfectionsByRequestedTime = (periodType, timeToElapse) => {
+  let noOfDays = 0;
+
+  switch (periodType) {
+    case 'days':
+      noOfDays = timeToElapse / 3;
+      break;
+
+    case 'weeks':
+      noOfDays = (timeToElapse * 7) / 3;
+      break;
+
+    case 'months':
+      noOfDays = (timeToElapse * 30) / 3;
+      break;
+
+    default:
+      break;
+  }
+
+  return (2 ** Math.trunc(noOfDays));
+};
+
 const covid19ImpactEstimator = (data) => {
   const input = data;
   return {
-    data: {},
-    impact: {},
-    severeImpact: {}
+    data: input,
+    impact: {
+      currentlyInfected: data.reportedCases * 10,
+      infectionsByRequestedTime: (data.reportedCases * 10)
+      * getInfectionsByRequestedTime(data.periodType, data.timeToElapse)
+    },
+    severeImpact: {
+      currentlyInfected: data.reportedCases * 50,
+      infectionsByRequestedTime: (data.reportedCases * 50)
+      * getInfectionsByRequestedTime(data.periodType, data.timeToElapse)
+    }
   };
 };
-impact.currentlyInfected = reportedCases * 10;
-severeImpact.currentlyInfected = reportedCases * 50;
+
 export default covid19ImpactEstimator;
